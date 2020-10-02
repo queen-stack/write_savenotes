0ee297cae7188ef8af45d73fb06699483de08417
JQueen <queen_jenf@yahoo.com>
2020-10-02 16:09:12 -0700

    updating server

--- server.js
+++ server.js
@@ -1,52 +1,95 @@
+// Dependencies
+// =============================================================
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
-// ShortId creates amazingly short non-sequential url-friendly unique ids. 
-// You'll need to find a way to give each note a unique id when it's saved.
 const shortid = require('shortid');
+const { formatWithOptions } = require('util');
 
+const app = express();
+const PORT = 3001;
 
-//let notes =[] experiment
 // Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));  // added v0.7
 
-// Data
+const debug = true;   // Added v0.7   -- set to false before committing!
+
+// Set up the data
 // =============================================================
-let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+let notes = JSON.parse(fs.readFileSync('./data/db.json'));
+
+// Write the "database" information back to disk
+// Added v0.7
+function updateDatabase() {
+  if (debug) {
+    console.log(JSON.stringify(notes));
+  }
+
+  fs.writeFileSync('./data/db.json', JSON.stringify(notes));
+}
 
 // Routes
 // =============================================================
-
-//Build route for new note
-app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-});
-//where is this popping up? terminal / browser
-
 // Return the Note Taker landing page.
 app.get('/', (req, res) => {
-  console.log("get \*"); //wildcard = '*' will return to the index if not found
-  return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+  if (debug) { console.log("get /"); }  // updated v0.7
+
+  res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 // Return the notes.html page
 app.get('/notes', (req, res) => {
-  console.log("get /notes");
-  return res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+
+  if (debug) { console.log("get /notes"); }  // updated v0.7
+
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 });
+
 // Return all the notes in the database
 app.get('/api/notes', (req, res) => {
-    console.log("get /api/notes");
-    let results = notes;
-    res.json(results);
-  });
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  
+
+  if (debug) { console.log("get /api/notes"); }  // updated v0.7
+
+  res.json(notes);
+});
+
+// Skeleton route for adding a new note.
+// Updated for v0.7.
+app.post('/api/notes', (req, res) => {
+
+  let newNote = req.body;
+  newNote.id = shortid.generate();
+
+  if (debug) { console.log('newNote.id = ' + newNote.id); }  // updated v0.7
+
+  notes.push(newNote); // Add new note to the in-memory "database"
+  updateDatabase();    // Update that database on disk
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+  let idToDelete = req.params.id;
+
+  if (debug) { console.log('Deleting note: ' + idToDelete); }  // updated v0.7
+
+  // Remove the note with the idToDelete from the in-memory database
+  notes = notes.filter(note => note.id !== idToDelete);
+  // And write the updated database to disk.
+  updateDatabase();
+
+  res.json(notes);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 // Listener
 // =============================================================
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
 });
-// routes don't matter till they are called
-//remember the path you created
